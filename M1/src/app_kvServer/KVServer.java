f1bce91332242d0cb70e950b6b1ef163bbc26898
An Yu <jerryyu8883@gmail.com>
2023-01-19 18:37:46 -0500

    server can make client connection and respond now

--- M1/src/app_kvServer/KVServer.java
+++ M1/src/app_kvServer/KVServer.java
@@ -25,7 +25,11 @@ public class KVServer extends Thread implements IKVServer  {
 	 *           currently not contained in the cache. Options are "FIFO", "LRU",
 	 *           and "LFU".
 	 */
+	private static Logger logger = Logger.getRootLogger();
+	
 	private int port;
+	private ServerSocket serverSocket;
+    private boolean running;
 
 	public KVServer(int port) {
 		// TODO Auto-generated method stub
@@ -35,6 +39,68 @@ public class KVServer extends Thread implements IKVServer  {
 	public KVServer(int port, int cacheSize, String strategy) {
 		// TODO Auto-generated method stub
 	}
+
+	/**
+     * Initializes and starts the server. 
+     * Loops until the the server should be closed.
+     */
+	@Override
+    public void run() {
+        
+    	running = initializeServer();
+        
+        if(serverSocket != null) {
+	        while(isRunning()){
+	            try {
+	                Socket client = serverSocket.accept();                
+	                ClientConnection connection = 
+	                		new ClientConnection(client);
+	                new Thread(connection).start();
+	                
+	                logger.info("Connected to " 
+	                		+ client.getInetAddress().getHostName() 
+	                		+  " on port " + client.getPort());
+	            } catch (IOException e) {
+	            	logger.error("Error! " +
+	            			"Unable to establish connection. \n", e);
+	            }
+	        }
+        }
+        logger.info("Server stopped.");
+    }
+
+	private boolean isRunning() {
+        return this.running;
+    }
+	    /**
+     * Stops the server insofar that it won't listen at the given port any more.
+     */
+    public void stopServer(){
+        running = false;
+        try {
+			serverSocket.close();
+		} catch (IOException e) {
+			logger.error("Error! " +
+					"Unable to close socket on port: " + port, e);
+		}
+    }
+
+    private boolean initializeServer() {
+    	logger.info("Initialize server ...");
+    	try {
+            serverSocket = new ServerSocket(port);
+            logger.info("Server listening on port: " 
+            		+ serverSocket.getLocalPort());    
+            return true;
+        
+        } catch (IOException e) {
+        	logger.error("Error! Cannot open server socket:");
+            if(e instanceof BindException){
+            	logger.error("Port " + port + " is already bound!");
+            }
+            return false;
+        }
+    }
 	
 	@Override
 	public int getPort(){
@@ -93,10 +159,6 @@ public class KVServer extends Thread implements IKVServer  {
 		// TODO Auto-generated method stub
 	}
 
-	@Override
-    public void run(){
-		// TODO Auto-generated method stub
-	}
 
 	@Override
     public void kill(){
