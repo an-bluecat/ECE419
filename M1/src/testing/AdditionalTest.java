033b3f72806b8ad3500e0149190c69239f53ba91
liming76 <mingh.li@mail.utoronto.ca>
2023-01-23 20:35:15 -0500

    added throughput evaluation to performance test

--- M1/src/testing/AdditionalTest.java
+++ M1/src/testing/AdditionalTest.java
@@ -4,6 +4,7 @@ import org.junit.Test;
 
 import junit.framework.TestCase;
 import app_kvServer.KVServer;
+import java.util.ArrayList;
 
 public class AdditionalTest extends TestCase {
 	
@@ -15,10 +16,11 @@ public class AdditionalTest extends TestCase {
 	 * @param itest is the upper bound for ikey as a percent, indicates the percentage of put requests
 	 * @param scaler is the number of keys to insert (pu)
 	 */
-	private void performanceTestHelper(int itest, int scaler) {
-		String value = "Test Content 2023";
+	private double performanceTestHelper(int itest, int scaler, String value) {
 
 		KVServer server = new KVServer(5000);
+		
+		double start = System.currentTimeMillis();
 		
 		// put
 		for (int ikey = 0; ikey <= scaler; ikey ++){ 
@@ -27,6 +29,7 @@ public class AdditionalTest extends TestCase {
 			try {
 				server.putKV(Integer.toString(ikey), value);
 			} catch (Exception e){
+				e.printStackTrace();
 				ex = e;
 			}
 			assertTrue(ex == null);
@@ -47,6 +50,10 @@ public class AdditionalTest extends TestCase {
 				limit --;
 			}
 		}
+		double elapsed = System.currentTimeMillis() - start;
+		double reqs = limit + scaler;
+	
+		return (elapsed / reqs);
 		
 	}
 
@@ -57,8 +64,27 @@ public class AdditionalTest extends TestCase {
 	 * 								puts 20% VS 80% gets
 	 */
 	public void testPerformance() {
-		for (int itest = 20; itest <= 80; itest += 20 ){
-			performanceTestHelper(itest, itest);
+//		String testString = "M1 performance test";
+		String testString = "Let me not to the marriage of true minds\r\n"
+				+ "Admit impediments. Love is not love\r\n"
+				+ "Which alters when it alteration finds,\r\n"
+				+ "Or bends with the remover to remove:\r\n"
+				+ "O, no! it is an ever-fixed mark,\r\n"
+				+ "That looks on tempests and is never shaken;\r\n"
+				+ "It is the star to every wandering bark,\r\n"
+				+ "Whose worth’s unknown, although his height be taken.\r\n"
+				+ "Love’s not Time’s fool, though rosy lips and cheeks\r\n"
+				+ "Within his bending sickle’s compass come;\r\n"
+				+ "Love alters not with his brief hours and weeks,\r\n"
+				+ "But bears it out even to the edge of doom.\r\n"
+				+ "If this be error and upon me proved,\r\n"
+				+ "I never writ, nor no man ever loved.";
+		
+		System.out.printf("Performance test average payloads\n");
+		for (int itest = 20; itest <= 80; itest += 10 ){
+			Double res = performanceTestHelper(itest, itest, testString);
+			System.out.printf("ratio of puts: " + Integer.toString(itest) + " is: "+ Double.toString(res)+"\n");
 		}
+	
 	}
 }
